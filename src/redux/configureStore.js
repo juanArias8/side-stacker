9d3cd66c57567cd3f167693dca13991adfa85bc0
juanArias8 <desarrolloingenieria18@udea.edu.co>
2021-02-16 00:20:31 -0500

    chore: change api url

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,12 +1,12 @@
 import {applyMiddleware, compose, createStore} from 'redux'
 import thunkMiddleware from 'redux-thunk'
-import loggerMiddleware from './logger'
 import chatReducer from "./reducer";
 
 
 export default function configureStore(preloadedState) {
-    const middlewares = [loggerMiddleware, thunkMiddleware]
-    const middlewareEnhancer = applyMiddleware(...middlewares)
+    // const middlewares = [loggerMiddleware, thunkMiddleware]
+    // const middlewareEnhancer = applyMiddleware(...middlewares)
+    const middlewareEnhancer = applyMiddleware(thunkMiddleware)
     const composedEnhancers = compose(middlewareEnhancer)
     return createStore(chatReducer, preloadedState, composedEnhancers)
 }
--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const API_URL = 'http://127.0.0.1:8000'
-export const WS_URL = 'ws://localhost:8000/ws'
+export const API_URL = 'https://side-stacker-game.herokuapp.com'
+export const WS_URL = 'wss://side-stacker-game.herokuapp.com/ws'
